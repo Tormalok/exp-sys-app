8c40cb6c65be14a6fe435358a6dabc67a0523d21
Henry Amponsah <tormalok@gmail.com>
2025-01-06 11:02:29 +0000

    Add profile component to homepage

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/Profile.css';
+
+const Profile = () => {
+  return (
+    <div className='profile'>
+      <div className='profile-img'></div>
+      <div className='message'>
+        <h1>Hello, John</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/Home.css';
 import Header from '../components/Header';
+import Profile from '../components/Profile';
 
 const Home = () => {
   return (
     <div className='bg-home'>
       <Header />
+      <Profile />
     </div>
   );
 };
