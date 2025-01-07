f54f15effb0afeaca60070cb6575b3ee9a9de479
Henry Amponsah <tormalok@gmail.com>
2025-01-07 09:43:30 -0800

    Add Sign In page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
     <div className='profile'>
       <div className='profile-img'></div>
       <div className='message'>
-        <h1>Hello!</h1>
+        <h1>Hello, have a go...</h1>
       </div>
     </div>
   );
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
+import './css/SignIn.css';
 
 const SignIn = () => {
-  return <div>Sign In Page</div>;
+  return (
+    <div className='sgn-in-bg'>
+      <div className='sgn-in-box'>
+        <h1>Welcome back</h1>
+        <form>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            placeholder='Enter your username*'
+            required
+          />
+
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='Enter your password*'
+            required
+          />
+
+          <button type='submit'>Sign In</button>
+        </form>
+
+        <h1 className='alt'>
+          Don't have an account?{' '}
+          <span>
+            <Link to='/signup'>Sign Up</Link>
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
