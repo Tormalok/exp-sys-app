386821f4d6110c2bc0f048d1f656701137106a46
Henry Amponsah <tormalok@gmail.com>
2025-01-07 09:57:10 -0800

    Add Sign Up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
+import './css/SignUp.css'; // Link to the new SignUp.css
 
 const SignUp = () => {
-  return <div>Sign Up Page</div>;
+  return (
+    <div className='sgn-up-bg'>
+      <div className='sgn-up-box'>
+        <h1>Create your account</h1>
+        <form>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='Enter your email*'
+            required
+          />
+
+          <input
+            type='text'
+            id='username'
+            name='username'
+            placeholder='Choose a username*'
+            required
+          />
+
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='Create a password*'
+            required
+          />
+
+          <button type='submit'>Sign Up</button>
+        </form>
+
+        <h1 className='alt'>
+          Already have an account?{' '}
+          <span>
+            <Link to='/signin'>Sign In</Link>
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
