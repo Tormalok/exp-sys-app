b4ff760822513e2e3bcd34924c795b4cc38cb9b8
Henry Amponsah <tormalok@gmail.com>
2025-01-07 11:28:14 -0800

    Add ResBox component to Home. Update Header component

---
+++ src/components/ResBox.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './css/ResBox.css';
+
+const ResBox = () => {
+  return <div className='rsb-bg'></div>;
+};
+
+export default ResBox;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,18 @@ import Header from '../components/Header';
 import Profile from '../components/Profile';
 import Option from '../components/Option';
 import Submit from '../components/Submit';
+import ResBox from '../components/ResBox';
 
 const Home = () => {
   return (
     <div className='bg-home'>
       <Header />
-      <Profile />
-      <Option />
-      <Submit />
+      <div className='content'>
+        <Profile />
+        <Option />
+        <Submit />
+      </div>
+      <ResBox />
     </div>
   );
 };
