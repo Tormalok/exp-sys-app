fc2ac5e1a03316987157da2697526008bb014c44
Henry Amponsah <tormalok@gmail.com>
2025-01-07 12:32:52 -0800

    Update ResBox component

--- src/components/ResBox.jsx
+++ src/components/ResBox.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './css/ResBox.css';
 
 const ResBox = () => {
-  return <div className='rsb-bg'></div>;
+  return (
+    <div className='rsb-bg'>
+      <div className='inner-box'>
+        <h1 className='rsb-title'>Response</h1>
+        <div className='rsb-main'></div>
+        <div className='btn'></div>
+      </div>
+    </div>
+  );
 };
 
 export default ResBox;
