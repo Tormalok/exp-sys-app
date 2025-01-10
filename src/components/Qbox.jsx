4a9ad361da5ee75cfbd8155bc7968c25594e9662
Henry Amponsah <tormalok@gmail.com>
2025-01-10 17:21:36 +0000

    Add qbox component to homepage

---
+++ src/components/Qbox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Qbox.css';
+
+const Qbox = () => {
+  return (
+    <div className='box-main'>
+      <div className='box-inner'></div>
+    </div>
+  );
+};
+
+export default Qbox;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/Home.css';
 import Header from '../components/Header';
+import Qbox from '../components/Qbox';
 
 const Home = () => {
   return (
     <div className='bg-home'>
       <Header />
+      <Qbox />
     </div>
   );
 };
